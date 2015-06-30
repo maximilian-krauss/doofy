31b56675cb0c6bfcb5447bf526e89b1ccd83b8b4
Maximilian Krauss <max@krauss.io>
2015-06-30 23:11:25 +0200

    Doofy now uses a reactiv chat client

---
+++ chat-client.js
@@ -0,0 +1,105 @@
+var _ = require('lodash'),
+    io = require('socket.io-client'),
+    Rx = require('rx'),
+    RxNode = require('rx-node'),
+    c = require('colors');
+
+var _socket;
+
+function ChatClient(url) {
+  this.url = url;
+  this.socket = undefined;
+  this.account = undefined;
+}
+
+var _getCurrentRooms = function(socket, callback) {
+  socket.emit('rooms:list', function(rooms) {
+    callback(rooms);
+  });
+};
+
+var _getFurtherRooms = function(socket, callback) {
+  socket.on('rooms:new', function(room) {
+    callback([ room ]);
+  });
+};
+
+var _hasBeenMentioned = function(account, message) {
+  return message.text.indexOf(account.username) > -1;
+};
+
+ChatClient.prototype.rooms = function() {
+  var getCurrentRoomsRx = Rx.Observable.fromCallback(_getCurrentRooms);
+  var getFurtherRoomsRx = Rx.Observable.fromCallback(_getFurtherRooms);
+
+  return Rx.Observable.concat(
+    getCurrentRoomsRx(this.socket),
+    getFurtherRoomsRx(this.socket)
+  );
+};
+
+ChatClient.prototype.subscribeToRooms = function(rooms) {
+  var that = this;
+
+  _.forEach(rooms, function(room) {
+    that.socket.emit('rooms:join', room.id, function(room) {
+      console.log('Room joined:'.gray, room.name.gray);
+    });
+  });
+};
+
+ChatClient.prototype.newMessages = function() {
+  var that = this;
+
+  return Rx.Observable.create(function(obs) {
+    that.socket.on('messages:new', function(message) {
+      if(message.owner.id === that.account.id) {
+        return;
+      }
+
+      obs.onNext({
+        message: message,
+        mentioned: _hasBeenMentioned(that.account, message),
+        you: that.account,
+        response: function(responseMessage) {
+          that.sendMessage(responseMessage, message.room.id);
+        }
+      });
+    });
+  });
+};
+
+ChatClient.prototype.sendMessage = function(message, room) {
+  this.socket.emit('messages:create', { text: message, room: room });
+};
+
+ChatClient.prototype.connect = function() {
+  var that = this;
+  that.socket = io.connect(that.url);
+
+  return Rx.Observable.create(function(observer) {
+
+    that.socket.on('connect', function() {
+
+      that.socket.emit('account:whoami', function(account) {
+        that.account = account;
+      });
+
+      observer.onNext();
+    });
+
+    that.socket.on('error', function(err) {
+      observer.onError(err);
+    });
+
+    that.socket.on('disconnect', function() {
+      observer.onCompleted();
+    });
+
+    return function() {
+      that.socket.disconnect();
+    }
+  });
+};
+
+module.exports = ChatClient;
--- doofy.js
+++ doofy.js
@@ -1,8 +1,8 @@
 var _ = require('lodash'),
-    io = require('socket.io-client'),
     colors = require('colors'),
     url = require('url'),
-    e = require('dotenv').load();
+    e = require('dotenv').load()
+    Client = require('./chat-client');
 
 var protocol = process.env.DOOFY_PROTOCOL
     hostname = process.env.DOOFY_HOSTNAME,
@@ -18,35 +18,26 @@ var chatUrl = url.format({
   }
 });
 
-var socket = io.connect(chatUrl),
-    currentProfile;
+var client = new Client(chatUrl);
 
-socket.on('connect', function() {
-  console.log('Connected!'.green);
+var clientSubscription = client.connect(chatUrl).subscribe(
+  function() {
+    console.log('connected');
 
-  socket.emit('rooms:list', function(rooms) {
-    _.forEach(rooms, function(room) {
-      socket.emit('rooms:join', room.id, function(room) {
-        console.log('Room joined:'.gray, room.name.gray);
+    client.rooms()
+      .subscribe(function(rooms) {
+        client.subscribeToRooms(rooms);
       });
-    });
+
+    client.newMessages()
+      .subscribe(function(message) {
+        console.log('new message arrived:'.green, message);
+
+        message.response('Ok, I get that!');
+      });
+  },
+  function(err) {
+    console.log('err', err);
+  }, function() {
+    console.log('completed');
   });
-
-  socket.emit('account:whoami', function(profile) {
-    currentProfile = profile;
-    console.log('Connected as:'.green, profile.username);
-  });
-
-});
-
-socket.on('error', function(err) {
-  console.log('Connection error:'.red, err);
-});
-
-socket.on('disconnect', function() {
-  console.log('Disconnected!'.yellow);
-});
-
-socket.on('messages:new', function(message) {
-  console.log(message);
-});
